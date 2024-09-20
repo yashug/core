10907633c575595ec57e5d5ac5e6b6fe5b67d151
2heal1 <TwoHeal@163.com>
2024-09-20 15:37:01 +0800

    chore: upgrade beta version

--- apps/modernjs-ssr-data-loader/host/src/entry-two/routes/page.tsx
+++ apps/modernjs-ssr-data-loader/host/src/entry-two/routes/page.tsx
@@ -1,16 +1,3 @@
-import { createRemoteSSRComponent } from '@modern-js/runtime/mf';
-
-const RemoteSSRComponent = createRemoteSSRComponent({
-  loader: () => import('host-provider/Image'),
-  loading: 'loading...',
-  export: 'Image',
-  fallback: ({ error }) => {
-    if (error instanceof Error && error.message.includes('not exist')) {
-      return <div>fallback - not existed id</div>;
-    }
-    return <div>fallback</div>;
-  },
-});
 const Index = () => (
   <div className="container-box">
     <h1>entry two page</h1>
